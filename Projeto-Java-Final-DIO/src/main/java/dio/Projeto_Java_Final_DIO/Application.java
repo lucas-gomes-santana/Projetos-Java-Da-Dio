557c7416276bf8ab3665ee57d45653495a6e489a
Lucas Gomes Santana <ls7128387@gmail.com>
2025-01-31 11:21:57 -0300

    Organizando o Repositório

---
+++ Projeto-Java-Final-DIO/src/main/java/dio/Projeto_Java_Final_DIO/Application.java
@@ -0,0 +1,13 @@
+package dio.Projeto_Java_Final_DIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Projeto-Java-Final-DIO/src/test/java/dio/Projeto_Java_Final_DIO/ProjetoJavaFinalDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.Projeto_Java_Final_DIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaFinalDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
